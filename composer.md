6a0366f1d8f676316fd2ab6eee66e11ce47d6c13
Frantz Kati <bahdcoder@gmail.com>
2025-01-23 09:48:23 +0100

    chore: add composer checklist

---
+++ composer.md
@@ -0,0 +1,63 @@
+# Composer v1 features
+
+1. Decent text editing experience
+
+- [ ] Bold marker
+- [ ] Underline marker
+- [ ] Strike through marker
+- [ ] Inline code marker
+
+2. Link editing
+
+- [ ] Add new Link
+- [ ] Edit existing link
+- [ ] Remove link
+- [ ] Select link presets (Contact unsubscribe, Contact preferences)
+
+3. Headings editing
+
+- [ ] Heading 1 with default styles
+- [ ] Heading 2 with default styles
+- [ ] Heading 3 with default styles
+
+4. Button editing
+
+- [ ] Change CSS properties of button
+- [ ] Edit button inline text
+- [ ] Add link to button
+
+5. Blocks drag and drop
+
+- [ ] Drag and drop blocks vertically
+
+6. Image uploads
+
+- [ ] Slash command upload image
+- [ ] Drag and drop image into editor
+- [ ] Resize uploaded image
+- [ ] Delete uploaded image
+- [ ] Set image alt description
+- [ ] Upload image from Upsplash
+
+7. Lists
+
+- [ ] Ordered Lists
+- [ ] Unordered Lists
+- [ ] Edit css properties on lists
+
+8. Horizontal line / Divider
+
+- [ ] Insert Divider
+- [ ] Remove Divider
+
+9. Editor content
+
+- [ ] All elements prefill into editor from database (when editing an existing broadcast and not creating one from scratch)
+- [ ] All elements are accurately converted to Foundation Email Framework Templates
+- [ ] All elements look good on mobile devices for a great mobile experience.
+
+10. Active node
+
+- [ ] Highlight active node with blue border
+- [ ] Add drag handle to active node
+- [ ] Add options dropdown to active node
--- pages/components/composer/composer.tsx
+++ pages/components/composer/composer.tsx
@@ -21,20 +21,18 @@ export function Composer() {
 
   return (
     <div className="w-full flex items-center h-full">
-      {false ? (
-        <div className="w-[16.25rem] h-full border-r kb-border-tertiary p-2">
-          <Tabs.Root defaultValue="layers" width="full">
-            <Tabs.List>
-              <Tabs.TabsTrigger value="layers">Layers</Tabs.TabsTrigger>
-              <Tabs.TabsTrigger value="blocks">Blocks</Tabs.TabsTrigger>
-              <Tabs.Indicator />
-            </Tabs.List>
+      <div className="w-[16.25rem] h-full border-r kb-border-tertiary p-2">
+        <Tabs.Root defaultValue="layers" width="full">
+          <Tabs.List>
+            <Tabs.TabsTrigger value="layers">Layers</Tabs.TabsTrigger>
+            <Tabs.TabsTrigger value="blocks">Blocks</Tabs.TabsTrigger>
+            <Tabs.Indicator />
+          </Tabs.List>
 
-            <Tabs.Content value="layers">Layers here</Tabs.Content>
-            <Tabs.Content value="blocks">Blocks here</Tabs.Content>
-          </Tabs.Root>
-        </div>
-      ) : null}
+          <Tabs.Content value="layers">Layers here</Tabs.Content>
+          <Tabs.Content value="blocks">Blocks here</Tabs.Content>
+        </Tabs.Root>
+      </div>
       <div
         className="flex-grow h-full p-6 overflow-y-auto w-composer-inter"
         ref={menuContainerRef}
@@ -55,11 +53,9 @@ export function Composer() {
           </div>
         </div>
       </div>
-      {false ? (
-        <div className="w-[16.25rem] h-full border-l kb-border-tertiary">
-          <BlockEditor editor={editor as Editor} />
-        </div>
-      ) : null}
+      <div className="w-[16.25rem] h-full border-l kb-border-tertiary">
+        <BlockEditor editor={editor as Editor} />
+      </div>
     </div>
   )
 }
