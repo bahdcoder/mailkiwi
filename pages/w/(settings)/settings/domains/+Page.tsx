a432605e935333981ed97fbedd37e4b88c8d72e2
Timi Ambaye <itstimiking@gmail.com>
2025-05-26 13:25:59 +0000

    update

--- pages/w/(settings)/settings/domains/+Page.tsx
+++ pages/w/(settings)/settings/domains/+Page.tsx
@@ -8,7 +8,7 @@ import { route } from '#root/core/shared/routes/route_aliases.js'
 function ProfilePage() {
   return (
     <div className="w-full max-w-2xl mx-auto py-4 lg:py-16 grid grid-cols-1 gap-y-4 p-4">
-      <Heading size="xs" variant="display">
+      <Heading size="lg" variant="heading">
         Domains
       </Heading>
 
--- pages/w/(settings)/settings/index/+Page.tsx
+++ pages/w/(settings)/settings/index/+Page.tsx
@@ -8,9 +8,10 @@ import { route } from '#root/core/shared/routes/route_aliases.js'
 function ProfilePage() {
   return (
     <div className="w-full max-w-2xl mx-auto py-4 lg:py-16 grid grid-cols-1 gap-y-4 p-4">
-      <Heading size="xs" variant="display">
+      <Heading size="lg" variant="heading">
         Profile
       </Heading>
+      <Text className="">Forem ipsum dolor sit amet, consectetur adipiscing elit</Text>
 
       <Divider />
     </div>
