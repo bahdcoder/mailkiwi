a6e301e4d99c0bc5a62787888da89ca594246050
Frantz Kati <bahdcoder@gmail.com>
2025-06-08 18:59:20 +0100

    chore: add pricing template

---
+++ pages/(site)/pricing/+Page.tsx
@@ -0,0 +1,9 @@
+function PricingPage() {
+  return (
+    <h1 className="flex items-center p-8 justify-center text-5xl">
+      This is the Pricing page
+    </h1>
+  )
+}
+
+export { PricingPage as Page }
